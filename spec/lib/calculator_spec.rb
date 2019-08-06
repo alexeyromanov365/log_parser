ddbe0fcd0519a730de17cb046c26a001f3696fdb
Alexey Romanov <aliaksei_ramanau@epam.com>
2019-08-06 12:54:05 +0300

    Calculator spec implemented

---
+++ spec/lib/calculator_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+require 'calculator'
+
+RSpec.describe Calculator do
+  let(:log_lines) do
+    [
+      Log::Line.new('/help_page/1 126.318.035.038'),
+      Log::Line.new('/help_page/1 126.318.035.035'),
+      Log::Line.new('/contact 184.123.665.067')
+    ]
+  end
+
+  subject { described_class.new(log_lines) }
+
+  describe '#calculate_visits' do
+    it 'returns url with calculated visits' do
+      expect(subject.visits).to eq([['/help_page/1', 2], ['/contact', 1]])
+    end
+  end
+
+  describe '#calculate_uniq_visits' do
+    it 'returns url with calculated uniq visits' do
+      expect(subject.uniq_visits).to eq([['/help_page/1', 2], ['/contact', 1]])
+    end
+  end
+end
