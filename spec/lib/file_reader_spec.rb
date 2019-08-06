b731a028b54767eea7f3d7ebc665f14740b8012d
Alexey Romanov <aliaksei_ramanau@epam.com>
2019-08-06 12:24:12 +0300

    FileReader spec

---
+++ spec/lib/file_reader_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+RSpec.describe FileReader do
+  subject { described_class.new('spec/fixtures/test_file.log') }
+
+  describe '#read' do
+    it 'returns data with url and ip' do
+      expect(subject.read.first).to eq("/help_page/1 126.318.035.038\n")
+    end
+
+    it 'returns array with urls and ips' do
+      expect(subject.read).to be_a(Array)
+    end
+  end
+end
+
