3a9908754a4836f07df22bd1df26373f8ec69838
Alexey Romanov <aliaksei_ramanau@epam.com>
2019-08-06 12:37:46 +0300

    Line spec

---
+++ spec/lib/line_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+RSpec.describe Line do
+  let(:valid_line) { '/help_page/1 126.318.035.038' }
+  let(:invalid_line) { '/help_page/1126.318.035.038' }
+
+  describe '#new' do
+    context 'with valid_line' do
+      it 'sets the url field' do
+        expect(described_class.new(valid_line).url).to eq(valid_line.split.first)
+      end
+
+      it 'sets the ip field' do
+        expect(described_class.new(valid_line).ip).to eq(valid_line.split.last)
+      end
+    end
+
+    context 'with invalid line' do
+      it 'sets the url field' do
+        expect(described_class.new(invalid_line).url).to eq(invalid_line.split.first)
+      end
+
+      it 'don\'t sets the ip field' do
+        expect(described_class.new(invalid_line).ip).to be_nil
+      end
+    end
+  end
+end
