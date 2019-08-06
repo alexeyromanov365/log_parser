193c8e7299138e283d284a0afa1590c240652229
Alexey Romanov <aliaksei_ramanau@epam.com>
2019-08-06 13:40:57 +0300

    Added Summary logic

--- lib/summary.rb
+++ lib/summary.rb
@@ -1,11 +1,27 @@
+require_relative 'log/reader'
+require_relative 'log/parser'
+require_relative 'calculator'
+require_relative 'printer'
+
 class Summary
   def initialize(path)
     @path = path
   end
 
   def visits
+    calculated_lines = Calculator.new(parsed_lines).calculate_visits
+    Printer.new(calculated_lines).visit_count
   end
 
   def uniq_visits
+    calculated_lines = Calculator.new(parsed_lines).calculate_uniq_visits
+    Printer.new(calculated_lines).uniq_visit_count
+  end
+
+  private
+
+  def parsed_lines
+    data = Log::Reader.new(@path).read
+    Log::Parser.new(data).parse
   end
 end
--- spec/lib/summary_spec.rb
+++ spec/lib/summary_spec.rb
@@ -1,18 +1,24 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 require 'summary'
+require 'pry'
 
 RSpec.describe Summary do
   subject { described_class.new('spec/fixtures/test_file.log') }
 
+  let(:visits_output) { "Total visits:\n/help_page/1 3 visits\n/contact 1 visits\n" }
+  let(:uniq_output) { "Uniq visits:\n/help_page/1 2 uniq visits\n/contact 1 uniq visits\n" }
+
   describe '#visits' do
     it 'returns url visits' do
-      expect(subject.visits).to eq('/help_page/1 3 visits, /contact 1 visits')
+      expect { subject.visits }.to output(visits_output).to_stdout
     end
   end
 
   describe '#uniq_visits' do
     it 'returns uniq url visits' do
-      expect(subject.uniq_visits).to eq('/help_page/1 2 uniq visits\n, /contact 1 uniq visits')
+      expect { subject.uniq_visits }.to output(uniq_output).to_stdout
     end
   end
 end
