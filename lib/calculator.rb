28d3172154c3a0329e2fdc53d1c157c02cb67286
Alexey Romanov <aliaksei_ramanau@epam.com>
2019-08-06 13:09:30 +0300

    Added Calculator

---
+++ lib/calculator.rb
@@ -0,0 +1,17 @@
+class Calculator
+  def initialize(lines)
+    @lines = lines
+  end
+
+  def visits
+    @lines.group_by(&:url).each_with_object({}) do |line, object|
+      object[line[0]] = line[1].size
+    end.sort_by(&:last).reverse
+  end
+
+  def uniq_visits
+    @lines.group_by(&:url).each_with_object({}) do |line, object|
+      object[line[0]] = line[1].map(&:ip).uniq.size
+    end.sort_by(&:last).reverse
+  end
+end
